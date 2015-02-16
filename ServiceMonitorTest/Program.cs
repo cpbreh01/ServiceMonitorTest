1ec4ad89c4996a19f6259a0eb58c5576e103fea0
chris <cpbreh01@gmail.com>
2015-02-16 18:58:45 -0500

    Initiall commit

---
+++ ServiceMonitorTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceMonitorTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ServiceControllerHelper> ListOfServices = new List<ServiceControllerHelper>();
+
+            //Create ServiceToMonitor Objects
+            ServiceControllerHelper ServiceToMonitor = new ServiceControllerHelper();
+
+            //Add ServiceToMonitorObjects to list.
+            ListOfServices.Add(ServiceToMonitor);
+            string _response = string.Empty;
+
+            while (!_response.Equals("n"))
+            {
+                Console.WriteLine("");
+
+                Console.WriteLine("Would you like to check status of service? 'Y' or 'N'");
+                _response = Console.ReadKey().KeyChar.ToString().ToLower();
+                Console.WriteLine("");
+                switch (_response)
+                {
+                    case "n":
+                        Console.Write("Exiting Program.");
+                        break;
+                    case "y":
+                        Console.Write(string.Format("Checking Services..."));
+                        foreach (ServiceControllerHelper serviceToCheck in ListOfServices)
+                        {
+                            Console.WriteLine(string.Format("Name: {0} Status: {1}", serviceToCheck.GetServiceName(), serviceToCheck.GetCurrentStatus()));
+                        }
+                        break;
+                    default:
+                        Console.Write(string.Format("Unknown response: {0}. No action taken. Use 'N' to exit", _response));
+                        break;
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ ServiceMonitorTest/ServiceControllerHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceProcess;
+
+namespace ServiceMonitorTest
+{
+    public class ServiceControllerHelper
+    {
+        private ServiceController monitoredService;
+
+        public ServiceControllerHelper()
+        {
+            string ServiceName = "dhcp";
+            monitoredService = new ServiceController(ServiceName, Environment.MachineName);
+        }
+
+        public string GetCurrentStatus()
+        {
+            ServiceControllerStatus _status = monitoredService.Status;
+
+            return _status.ToString();
+            
+        }
+
+        public string GetServiceName()
+        {
+            return monitoredService.DisplayName;
+        }
+      
+    }
+}
